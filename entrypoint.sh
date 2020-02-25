6052f622a590ce05e9ee5d7dacd22d79d5cd044c
César M. Cristóbal <cmartin@paradigmadigital.com>
2020-02-25 12:53:13 +0100

    Remove src and docker image base

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source /app/venv/bin/activate
-echo $INPUT_SA_KEY | base64 -di > service_account.json
-envsubst < inventory.gcp.tpl.yml | tee inventory.gcp.yml
-gcloud auth activate-service-account --key-file=service_account.json
-ansible-playbook -i inventory.gcp.yml playbook.yaml
