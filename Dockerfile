7a43b6a6818d091e83070702d3a6ce1ad73762f2
César M. Cristóbal <cmartin@paradigmadigital.com>
2020-03-03 16:29:07 +0100

    Remove entrypoint test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,1 @@
 FROM jilgue/gcp-dev-ide-docker-action-image:develop
-
-COPY entrypoint.sh /app/entrypoint.sh
-ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-INSTANCE_NAME=$(echo "$GITHUB_REPOSITORY-$INPUT_GITHUB_ACTOR_EMAIL" | perl -ne 'print lc' | perl -pe 's/.*\/(.*)/$1/s' | perl -pe 's/"//gm' | perl -pe 's/((?![a-z0-9]).)/-/gm' | perl -pe 's/.*(-)$//gm' )
-export INSTANCE_NAME
-
-printenv
